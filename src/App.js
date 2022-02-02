b86095793f75825e5e932d8f4d9071aac50bf426
Aurélien <aurelien.strimelle@bstorm.be>
2022-02-02 08:56:52 +0100

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 import Reservoir from './components/reservoir/reservoir';
 
 function App() {
+
+  const [message, setMessage] = useState('');
+
+
   return (
     <div className="App">
       <h1>Demo 05 - La communication</h1>
-      <Reservoir  />
+
+      <h2>Gestion du reservoir</h2>
+      <Reservoir />
+
+      {message && (
+        <p>{message}</p>
+      )}
     </div>
   );
 }
--- src/components/reservoir/reservoir.jsx
+++ src/components/reservoir/reservoir.jsx
@@ -1,21 +1,21 @@
 import { useState } from 'react';
 
 const Reservoir = (props) => {
-    const {max} = props;
+    const { max } = props;
 
     const [niveau, setNiveau] = useState(0);
 
     const handleUp = () => {
-        if(niveau < 10) {
+        if (niveau < 10) {
             setNiveau(n => n + 1);
         }
-    }
+    };
 
     const handleDown = () => {
-        if(niveau > 0) {
+        if (niveau > 0) {
             setNiveau(n => n - 1);
         }
-    }
+    };      
 
     return (
         <div>
@@ -23,11 +23,11 @@ const Reservoir = (props) => {
             <button onClick={handleUp}>Ajouter 1L</button>
             <button onClick={handleDown}>Retirer 1L</button>
         </div>
-    )
-}
+    );
+};
 
 Reservoir.defaultProps = {
     max: 10
-}
+};
 
 export default Reservoir;
